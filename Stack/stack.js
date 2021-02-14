acb60d264e8eec8a7792f60b8d6876abe2fc170b
mzs21 <snigdho2111@gmail.com>
2021-02-14 13:26:48 +0600

    🥞 Stack Data Structure

---
+++ Stack/stack.js
@@ -0,0 +1,114 @@
+class Stack
+{
+    constructor()
+    {
+        this.items = [];                                        
+        this.count = 0;                                         
+    }
+
+    // Add element to the top of the stack
+    
+    push(element)
+    {
+        this.items[this.count] = element;
+        console.log(`${element} added to ${this.count}`)
+        this.count += 1;
+        return this.count - 1;
+    }
+
+    // Return and remove top element in stack
+    // Return undefined if stack is empty
+
+    pop()
+    {
+        if(this.count == 0) return undefined;
+
+        let deleteItm = this.items[this.count - 1];
+        console.log(`${deleteItm} removed`)
+        this.count--;
+        return deleteItm;
+    }
+
+    // Check top element in stack
+
+    peek()
+    {
+        console.log(`Top element in stack ${this.items[this.count - 1]}`);
+        return this.items[this.count - 1];
+    }
+
+    // Check if stack is empty
+
+    isEmpty()
+    {
+        console.log(this.count == 0 ? 'Stack is empty' : 'Stack is NOT empty');
+        return this.count == 0;
+    }
+
+    // Stack size
+
+    size()
+    {
+        console.log(`Stack size ${this.count}`)
+        return this.count;
+    }
+
+    print()
+    {   
+        let str = '';
+
+        if(this.count != 0)
+        {
+            for(let i = 0; i< this.count; i++)
+            {
+                str += this.items[i] + ' ';
+            }
+        }
+        else{str = '[EMPTY]'}
+        
+        return str;
+    }
+
+    // Clear stack
+
+    clear()
+    {
+        this.items = [];
+        this.count = 0;
+
+        console.log('Stack cleared...');
+
+        return this.items;
+    }
+}
+
+// Time Complexity O(1)
+// Space Complexity O(n)
+
+// The lines below are not part of the data structure
+
+/*
+const stack = new Stack();
+
+
+stack.isEmpty();
+stack.push(100);
+stack.push(200);
+
+stack.peek();
+stack.push(300);
+
+console.log('Stack: ' + stack.print())
+
+stack.pop();
+
+stack.size();
+
+console.log('Stack: ' + stack.print())
+
+stack.clear();
+stack.size();
+
+console.log('Stack: ' + stack.print())
+
+*/
