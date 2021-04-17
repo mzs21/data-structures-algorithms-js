f5da775bca914579b9cbb50d4115d22cf33da236
mzs21 <snigdho2111@gmail.com>
2021-04-18 02:59:21 +0600

    🔍 Binary Search Algorithm updated

--- Searching/binarySearch.js
+++ Searching/binarySearch.js
@@ -4,19 +4,15 @@ const binarySearch = (ara,find) =>
     let right = ara.length;
     let mid;
 
-    let newAra = ara.sort((a,b) => a-b);            // These two lines are not part of the
-    console.log(newAra.join(' '));                  // algorithm but adding these will be better
-
-
-    while(left <= right)                            // Will check if 'left is '<=' to right, if it's true then,
+    while(left <= right)                            // If 'left is '<=' to right, if it's true then,
     {
         mid = Math.floor((left + right) / 2);       // we'll set the value of 'mid' to summation of left & right, divided by 2
-        if (newAra[mid] == find) {return mid;}      // If the value located in the midth index of array is euqal to our desired 
+        if (ara[mid] == find) return mid;           // If the value located in the midth index of array is euqal to our desired 
                                                     // value then we'll return the index
 
-        if (newAra[mid] < find) {left = mid + 1}    // If the first 'if' fails, then we'll check if the midth index of array is 
+        if (ara[mid] < find) left = mid + 1;        // If the first 'if' fails, then we'll check if the midth index of array is 
                                                     // less than our desired value and set the value of 'left' to mid + 1
-        else {right = mid - 1}                      // Otherwise we'll set the value of 'right' to mid - 1;
+        else right = mid - 1;                       // Otherwise we'll set the value of 'right' to mid - 1;
     }
     return true;                                    // If the 'while' condition fails, we'll just return true
 }
@@ -34,20 +30,18 @@ const newStr = arr.split(' '); // Will convert into an array
 
 const newArray = []; // Empty array
 
-for (let i = 0; i < newStr.length; i++) {
+for (let i = 0; i < newStr.length; i++) 
+{
     newArray.push(Number(newStr[i]));  // Will convert the string elements to numbers
 }
 
-const element = Number(input('The element I want to find: '));
+console.log(`Array: ${newArray.join(' ')}`); // This array will be of integers
 
-const findNumber = binary_Search(newArray, element);
+const element = Number(input(`The element I want to find: `));
 
-if (findNumber !== true) 
-{
-    console.log("Number is found at index " + Number(findNumber + 1));
-}
-else 
-{
-     console.log("Number is not found") 
-};
+const findNumber = binarySearch(newArray, element);
+
+if (findNumber !== true) console.log(`Number is found at index: ${Number(findNumber)}`);
+
+else console.log(`Number is not found`);
 */
